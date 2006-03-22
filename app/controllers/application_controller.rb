0cd2c9716f3ae87a811c6ec0b730826e99b3527f
altimont <>
2006-03-22 19:16:20 +0000

    Added verify_manager_admin_privs() and updated header documentation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+########################################################################
+#
+# Copyright 2006, by Teradyne, Inc., Boston MA
+#
+# File: application.rb
+#
 # The filters added to this controller will be run for all controllers in the application.
 # Likewise will all the methods added be available for all controllers.
+#
+# $Id$
+#
+########################################################################
+
 
 require_dependency "login_system"
 
@@ -8,6 +19,8 @@ class ApplicationController < ActionController::Base
   include LoginSystem
   model :user
 
+  # Do not allow any of the following listed actions unless the user is logged
+  # in.
   before_filter :login_required, :only => [:append,
                                            :copy, 
                                            :destroy,
@@ -20,6 +33,21 @@ class ApplicationController < ActionController::Base
 
 
 
+  ######################################################################
+  #
+  # paginate_collection
+  #
+  # Description:
+  # Updates the based on user input on the admin update screen.
+  #
+  # Parameters from @params
+  #
+  # Return value:
+  # pages - the paged listing.
+  # slice - identifies the first and last element on the page from pages.
+  #
+  ######################################################################
+  #
   def paginate_collection(collection, options ={})
     default_options = {:per_page => 15, :page => 1}
     options = default_options.merge options
@@ -34,13 +62,64 @@ class ApplicationController < ActionController::Base
     return [pages, slice]
   end
 
+  
   private
 
+  ######################################################################
+  #
+  # verify_admin_role
+  #
+  # Description:
+  # Verifies that the user is an administrator
+  #
+  # Return value:
+  # TRUE if the user is an admin, false otherwise
+  #
+  # Additional Information:
+  # Set flash['notice'] if the user is not an admin for the message to
+  # be displayed.
+  #
+  ######################################################################
+  #
   def verify_admin_role
     unless session[:active_role] == 'Admin'
       flash['notice'] = Pcbtr::MESSAGES[:admin_only]
       redirect_to(:controller => 'tracker',
                   :action     => "index")
+    end
+  end
+
+  ######################################################################
+  #
+  # verify_manager_admin_privs
+  #
+  # Description:
+  # Verifies that the user is either an administrator or a PCB manager.
+  #
+  # Return value:
+  # TRUE if the user is an admin or a PCB manager, false otherwise
+  #
+  # Additional Information:
+  # Set flash['notice'] if the user is not an admin or PCB Manager for the 
+  # message to be displayed.
+  #
+  ######################################################################
+  #
+  def verify_manager_admin_privs
+    admin   = Role.find_by_name('Admin')
+    manager = Role.find_by_name('Manager')
+
+    begin
+      unless session[:roles].include?(admin) ||
+             session[:roles].include?(manager)
+        flash['notice'] = 'Access not allowed'
+        redirect_to(:controller => 'tracker',
+                    :action     => "index")
+      end
+    rescue
+      flash['notice'] = 'Update not allowed - Must be admin or manager'
+      redirect_to(:controller => 'tracker',
+                    :action     => "index")
     end
   end
 
