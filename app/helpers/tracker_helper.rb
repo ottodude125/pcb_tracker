c95f6020f913a54698f02b7db40c3312b431f3f4
altimont <>
2006-03-22 16:01:07 +0000

    Updated review_locked to ust the designer_complete? and auditor_complete? attributes in place of calculating the same information.  Corrected the logic used to compute the return value so that it is now correct.
    Reference DSR 6683.

--- app/helpers/tracker_helper.rb
+++ app/helpers/tracker_helper.rb
@@ -12,18 +12,9 @@ module TrackerHelper
 
 
   def review_locked(design_review)
-
     audit = design_review.design.audit
-    total_designer_checks = 
-      audit.checklist.designer_only_count + audit.checklist.designer_auditor_count
-    designer_done = audit.designer_completed_checks == total_designer_checks
-    auditor_done  = audit.auditor_completed_checks == audit.checklist.designer_auditor_count
-
     is_final = design_review.review_type.name == "Final"
-
-    is_final && !designer_done &&  !auditor_done
-    
-
+    is_final && !(audit.designer_complete? && audit.auditor_complete?)
   end
 
 
