e3f1f0139e37a86543a977afc19f4b4f7fc52245
altimont <>
2006-05-24 16:55:27 +0000

    Updated the tests to complete testing.

--- test/functional/tracker_controller_test.rb
+++ test/functional/tracker_controller_test.rb
@@ -35,7 +35,7 @@ class TrackerControllerTest < Test::Unit::TestCase
 
 
   def test_1_id
-    print ("\n*** Tracker Controller Test - NEEDS WORK!!!\n")
+    print ("\n*** Tracker Controller Test\n")
     print ("*** $Id$\n")
   end
 
@@ -50,6 +50,10 @@ class TrackerControllerTest < Test::Unit::TestCase
   ######################################################################
   #
   def test_manager_home
+  
+    post('manager_home')
+    assert_response(302)
+    assert_redirected_to(:controller => 'tracker', :action => 'index')
     
     set_user(users(:jim_l).id, 'Manager')
     post('manager_home')
@@ -58,6 +62,80 @@ class TrackerControllerTest < Test::Unit::TestCase
 
     design_reviews = assigns(:design_reviews)
     assert_equal(3, design_reviews.size)
+
+    expected_design_reviews = [ DesignReview.find(design_reviews(:la455b_placement).id),
+                                DesignReview.find(design_reviews(:mx600a_pre_artwork).id),
+                                DesignReview.find(design_reviews(:mx700b_release).id)]
+    expected_design_reviews =
+      expected_design_reviews.sort_by { |review| review.priority.value }
+    expected_design_reviews.reverse!
+
+    assert_equal(expected_design_reviews, design_reviews)
+    
+    post('manager_list_by_priority', :order => 'DESC')
+    expected_design_reviews.reverse!
+    assert_equal('ASC',                   assigns(:sort_order)[:priority])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    expected_design_reviews.reverse!
+    post('manager_list_by_priority', :order => 'ASC')
+    assert_equal('DESC',                  assigns(:sort_order)[:priority])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    post('manager_list_by_design', :order => 'DESC')
+    expected_design_reviews = 
+      expected_design_reviews.sort_by { |design_review| design_review.design.name }
+    assert_equal('ASC',                   assigns(:sort_order)[:design])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    expected_design_reviews.reverse!
+    post('manager_list_by_design', :order => 'ASC')
+    assert_equal('DESC',                  assigns(:sort_order)[:design])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    post('manager_list_by_type', :order => 'DESC')
+    expected_design_reviews = 
+      expected_design_reviews.sort_by { |design_review| design_review.review_type.name }
+    assert_equal('ASC',                   assigns(:sort_order)[:type])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    expected_design_reviews.reverse!
+    post('manager_list_by_type', :order => 'ASC')
+    assert_equal('DESC',                  assigns(:sort_order)[:type])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    post('manager_list_by_designer', :order => 'DESC')
+    expected_design_reviews = 
+      expected_design_reviews.sort_by { |design_review| User.find(design_review.designer_id).last_name }
+    assert_equal('ASC',                   assigns(:sort_order)[:designer])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    expected_design_reviews.reverse!
+    post('manager_list_by_designer', :order => 'ASC')
+    assert_equal('DESC',                  assigns(:sort_order)[:designer])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    post('manager_list_by_peer', :order => 'DESC')
+    expected_design_reviews = 
+      expected_design_reviews.sort_by { |design_review| User.find(design_review.design.peer_id).last_name }
+    assert_equal('ASC',                   assigns(:sort_order)[:peer])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    expected_design_reviews.reverse!
+    post('manager_list_by_peer', :order => 'ASC')
+    assert_equal('DESC',                  assigns(:sort_order)[:peer])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    post('manager_list_by_date', :order => 'DESC')
+    expected_design_reviews = 
+      expected_design_reviews.sort_by { |design_review| design_review.reposted_on }
+    assert_equal('ASC',                   assigns(:sort_order)[:date])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
+
+    expected_design_reviews.reverse!
+    post('manager_list_by_date', :order => 'ASC')
+    assert_equal('DESC',                  assigns(:sort_order)[:date])
+    assert_equal(expected_design_reviews, assigns(:design_reviews))
 
   end
   
@@ -117,10 +195,14 @@ class TrackerControllerTest < Test::Unit::TestCase
   ######################################################################
   #
   def test_designer_home
+
+    post('designer_home')
+    assert_response(302)
+    assert_redirected_to(:controller => 'tracker', :action => 'index')
     
     set_user(users(:bob_g).id, 'Designer')
     get :index
-    assert_response 302
+    assert_response(302)
     assert_redirected_to(:action => :designer_home)
 
     #follow_redirect
@@ -138,39 +220,20 @@ class TrackerControllerTest < Test::Unit::TestCase
   ######################################################################
   #
   def test_reviewer_home
-    print '?'
-    assert true
+
+    post('reviewer_home')
+    assert_response(302)
+    assert_redirected_to(:controller => 'tracker', :action => 'index')
+    
+    set_user(users(:lee_s).id, 'Reviewer')
+    get :index
+    assert_response(302)
+    assert_redirected_to(:action => :reviewer_home)
+
+    #follow_redirect
+    #assert_no_tag :content => "POST Placement Review"
+
   end
   
-  
-  ######################################################################
-  #
-  # test_admin_home
-  #
-  # Description:
-  # This method does the functional testing for the admin methods
-  #
-  ######################################################################
-  #
-  def test_admin_home
-    print '?'
-    assert true
-  end
-  
-  
-  ######################################################################
-  #
-  # test_get_design_reviews
-  #
-  # Description:
-  # This method does the functional testing for the get_design_review
-  # method
-  #
-  ######################################################################
-  #
-  def test_get_design_reviews
-    print '?'
-    assert true
-  end
   
 end
