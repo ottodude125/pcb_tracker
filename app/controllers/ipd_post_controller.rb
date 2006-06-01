f139e7341cd2e0854515549c99a51bef62deb888
altimont <>
2006-06-01 20:54:27 +0000

    Modifications to create().
            1. Removed the call to TrackerMailer::deliver_ipd_update().  It it
               handled later in the flow.
            2. Fixed the hash key for the flash notice.
            3. Changed the redirect from the show action to the manage_email_list
               action.
    
    Added the following methods.
    
            1. manage_email_list() to set up the data for the Manage Email List
               view for a newly created thread.
            2. add_to_thread_list() to add the selected user to the thread's CC
               list and set up the data to redisplay the CC list section of the
               Manage Email List view.
            3. remove_from_thread_list() to remove the selected user from the
               thread's CC list and set up the data to redisplay the CC list
               section of the Manage Email List view.
            4. deliver_thread_mail() to send the mail after the user has clicked
               the send button on the Manage Email List view.
            5. modify_email_list() to set up the data for the Manager Email List
               view for an existinig thread.
    DSR 6655

--- app/controllers/ipd_post_controller.rb
+++ app/controllers/ipd_post_controller.rb
@@ -142,13 +142,229 @@ class IpdPostController < ApplicationController
     @ipd_post = IpdPost.new(params[:ipd_post])
     @ipd_post.user_id = @session[:user].id
     if @ipd_post.save
-      TrackerMailer::deliver_ipd_update(@ipd_post)
-      flash[:notice] = 'Post was successfully created'
-      redirect_to(:action => 'show', :id => @ipd_post.id)
+      flash['notice'] = 'Post was successfully created'
+      redirect_to(:action => 'manage_email_list',
+                  :id     => @ipd_post.id)
     else
       render :action => 'new'
     end
   end
   
   
+  ######################################################################
+  #
+  # manage_email_list
+  #
+  # Description:
+  # This method sets up the data structures to manage the email list of
+  # a new thread.
+  #
+  # Parameters from @params
+  # id - identifies the root ipd post.
+  #
+  ######################################################################
+  #
+  def manage_email_list
+
+    flash['notice'] = flash['notice']
+  
+    @posting_new_thread = true
+    @ipd_post           = IpdPost.find(params[:id])
+    @associated_users   = @ipd_post.design.get_associated_users_by_role
+
+    @manager_list = Role.find_by_name('Manager').users
+    @manager_list.delete_if { |user| not user.active? }
+   
+    @input_gate_list = Role.find_by_name('PCB Input Gate').users
+    @input_gate_list.delete_if { |user| not user.active? }
+    
+    @optional_cc_list = @ipd_post.users.dup
+    
+
+    available_to_cc = User.find_all('active=1')
+    available_to_cc.delete_if { |user| user == @session[:user] }
+    available_to_cc.delete_if { |user| user == @associated_users['HWENG'] }
+    available_to_cc.delete_if { |user| user == @associated_users['Hardware Engineering Manager'] }
+    for input_gate in @input_gate_list
+      available_to_cc.delete_if { |user| user == input_gate }
+    end
+    for manager in @manager_list
+      available_to_cc.delete_if { |user| user == manager }
+    end
+    for person in @optional_cc_list
+      available_to_cc.delete_if { |user| user == person }
+    end
+    @available_to_cc = available_to_cc.sort_by { |user| user.last_name }
+    
+    email_list = {
+      :optional_cc_list => @optional_cc_list,
+      :available_to_cc  => @available_to_cc
+      }
+    flash[:thread_email_list] = email_list
+    flash[:ipd_post]          = @ipd_post
+    
+  end
+  
+  
+  ######################################################################
+  #
+  # add_to_thread_list
+  #
+  # Description:
+  # This method adds the user identified by the id paramater to the 
+  # thread's email CC list.
+  #
+  # Parameters from @params
+  # id - the id of the user record to add to the list.
+  #
+  ######################################################################
+  #
+  def add_to_thread_list
+  
+    email_list        = flash[:thread_email_list]
+    @optional_cc_list = email_list[:optional_cc_list]
+    @available_to_cc  = email_list[:available_to_cc]
+    ipd_post          = flash[:ipd_post]
+
+    user_to_add = @available_to_cc.detect { |user| user.id == @params[:id].to_i }
+    @available_to_cc.delete_if { |user| user == user_to_add }
+
+    ipd_post.users << user_to_add
+
+    @optional_cc_list << user_to_add
+    @optional_cc_list = @optional_cc_list.sort_by { |user| user.last_name }
+
+    email_list = {
+      :optional_cc_list => @optional_cc_list,
+      :available_to_cc  => @available_to_cc
+    }
+
+    flash[:thread_email_list] = email_list
+    flash[:ipd_post]          = ipd_post
+
+    render(:action => 'update_thread_list',
+           :layout => false)
+
+  end
+  
+  
+  ######################################################################
+  #
+  # remove_from_thread_list
+  #
+  # Description:
+  # This method removes the user identified by the id paramater from the 
+  # thread's email CC list.
+  #
+  # Parameters from @params
+  # id - the id of the user record to remove from the list.
+  #
+  ######################################################################
+  #
+  def remove_from_thread_list
+  
+    email_list        = flash[:thread_email_list]
+    @optional_cc_list = email_list[:optional_cc_list]
+    @available_to_cc  = email_list[:available_to_cc]
+    ipd_post          = flash[:ipd_post]
+
+    user_to_remove = @optional_cc_list.detect { |user| user.id == @params[:id].to_i }
+    @optional_cc_list.delete_if { |user| user == user_to_remove}
+
+    ipd_post.remove_users(user_to_remove)
+
+    @available_to_cc << user_to_remove
+    @available_to_cc = @available_to_cc.sort_by { |user| user.last_name }
+    
+    email_list = {
+      :optional_cc_list => @optional_cc_list,
+      :available_to_cc  => @available_to_cc
+    }
+    
+    flash[:thread_email_list] = email_list
+    flash[:ipd_post]          = ipd_post
+    
+    render(:action => 'update_thread_list',
+           :layout => false)
+    
+  end
+  
+  
+  ######################################################################
+  #
+  # deliver_thread_mail
+  #
+  # Description:
+  # This method delivers the email and then redirects back to the IPD
+  # post.
+  #
+  # Parameters from @params
+  # id - identifies the root ipd post.
+  #
+  ######################################################################
+  #
+  def deliver_thread_mail
+    ipd_post = IpdPost.find(params['ipd_post']['id'])
+    TrackerMailer::deliver_ipd_update(ipd_post)
+    redirect_to(:action => 'show', :id => ipd_post.id)
+  end
+  
+  
+  ######################################################################
+  #
+  # modify_email_list
+  #
+  # Description:
+  # This method sets up the data structures to manage the email list
+  # of an existing thread.
+  #
+  # Parameters from @params
+  # id - identifies the root ipd post.
+  #
+  ######################################################################
+  #
+  def modify_email_list
+
+    flash['notice'] = flash['notice']
+  
+    @posting_new_thread = false
+    @ipd_post           = IpdPost.find(params[:ipd_post_id])
+    @associated_users   = @ipd_post.design.get_associated_users_by_role
+
+    @manager_list = Role.find_by_name('Manager').users
+    @manager_list.delete_if { |user| not user.active? }
+   
+    @input_gate_list = Role.find_by_name('PCB Input Gate').users
+    @input_gate_list.delete_if { |user| not user.active? }
+    
+    @optional_cc_list = @ipd_post.users.dup
+    
+
+    available_to_cc = User.find_all('active=1')
+    available_to_cc.delete_if { |user| user == @session[:user] }
+    available_to_cc.delete_if { |user| user == @associated_users['HWENG'] }
+    available_to_cc.delete_if { |user| user == @associated_users['Hardware Engineering Manager'] }
+    for input_gate in @input_gate_list
+      available_to_cc.delete_if { |user| user == input_gate }
+    end
+    for manager in @manager_list
+      available_to_cc.delete_if { |user| user == manager }
+    end
+    for person in @optional_cc_list
+      available_to_cc.delete_if { |user| user == person }
+    end
+    @available_to_cc = available_to_cc.sort_by { |user| user.last_name }
+    
+    email_list = {
+      :optional_cc_list => @optional_cc_list,
+      :available_to_cc  => @available_to_cc
+      }
+    flash[:thread_email_list] = email_list
+    flash[:ipd_post]          = @ipd_post
+    
+    render(:action => 'manage_email_list')
+    
+  end
+
+
 end
