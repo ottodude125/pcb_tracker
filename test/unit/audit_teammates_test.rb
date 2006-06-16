52b6ae8e893b46f51c3e07a8009583abbc3e654e
altimont <>
2006-06-16 19:05:03 +0000

    The initial audit teammate model test template.
    DSR 6827

---
+++ test/unit/audit_teammates_test.rb
@@ -0,0 +1,23 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: audit_teammates_test.rb
+#
+# This file contains the unit tests for the audit teammates model
+#
+# Revision History:
+#   $Id$
+#
+########################################################################
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AuditTeammatesTest < Test::Unit::TestCase
+  fixtures :audit_teammates
+
+  # Replace this with your real tests.
+  def test_truth
+    #assert_kind_of AuditTeammates, audit_teammates(:first_teammate)
+  end
+end
