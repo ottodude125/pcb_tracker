0863189ae17885519a6219510a4dc69a78013cd6
altimont <>
2006-06-17 11:30:13 +0000

    Added the relationship to audit teammates.
    DSR 6827

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,21 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: section.rb
+#
+# This file maintains the state for sections.
+#
+# $Id$
+#
+########################################################################
+
 class Section < ActiveRecord::Base
+
   belongs_to :checklist
+  
+  has_many   :audit_teammates
   has_many   :checks
   has_many   :subsections
+  
 end
