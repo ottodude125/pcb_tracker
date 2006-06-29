d07b667b83babcc267af3e940200766251d439aa
altimont <>
2006-06-29 15:43:32 +0000

    Added the header comment.
    Added the relationship with user.

--- app/models/design_review_comment.rb
+++ app/models/design_review_comment.rb
@@ -1,2 +1,17 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: designe_review_comment.rb
+#
+# This file maintains the state for users.
+#
+# $Id$
+#
+########################################################################
+
 class DesignReviewComment < ActiveRecord::Base
+
+  belongs_to :user
+
 end
