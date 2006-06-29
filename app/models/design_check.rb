a07f103fbec79e31a576e3ed8eab161f52d50163
altimont <>
2006-06-29 15:41:30 +0000

    Modified the relationship with audit_comments.
    Added peer_auditor() and self_auditor()

--- app/models/design_check.rb
+++ app/models/design_check.rb
@@ -1,8 +1,63 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: design_check.rb
+#
+# This file maintains the state for design checks.
+#
+# $Id$
+#
+########################################################################
+
 class DesignCheck < ActiveRecord::Base
 
   belongs_to :audit
   belongs_to :check
  
 
-  has_many :audit_comments
+  has_many(:audit_comments, :order => 'created_on DESC')
+  
+  
+  ######################################################################
+  #
+  # peer_auditor
+  #
+  # Description:
+  # Provides a user record for the peer auditor
+  #
+  # Return value:
+  # A user record
+  #
+  ######################################################################
+  #
+  def peer_auditor
+    if self.auditor_id > 0 
+      User.find(self.auditor_id)
+    else
+      User.new(:first_name => 'Not', :last_name => 'Assigned')
+    end
+  end
+  
+  
+  ######################################################################
+  #
+  # self_auditor
+  #
+  # Description:
+  # Provides a user record for the self auditor
+  #
+  # Return value:
+  # A user record
+  #
+  ######################################################################
+  #
+  def self_auditor
+    if self.designer_id > 0
+      User.find(self.designer_id)
+    else
+      User.new(:first_name => 'Not', :last_name => 'Assigned')
+    end
+  end
+  
 end
