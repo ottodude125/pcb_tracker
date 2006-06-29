e85bef270841fc3e1c9544b6897a28cd23001512
altimont <>
2006-06-29 16:23:44 +0000

    Added the header comment.
    Updated the relationship with checks.
    
    Added designer_auditor_checks.

--- app/models/subsection.rb
+++ app/models/subsection.rb
@@ -1,5 +1,45 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: subsection.rb
+#
+# This file maintains the state for subsections.
+#
+# $Id$
+#
+########################################################################
+
 class Subsection < ActiveRecord::Base
+
   belongs_to :checklist
   belongs_to :section
-  has_many   :checks
+  
+  has_many(:checks,     :order => 'sort_order ASC')
+  
+  
+  ######################################################################
+  #
+  # designer_auditor_checks
+  #
+  # Description:
+  # This method returns the number of checks in the subsection.
+  #
+  # Parameters:
+  # None
+  #
+  ######################################################################
+  #
+  def designer_auditor_checks
+    
+    total = 0
+    
+    for check in self.checks
+      total += 1 if check.check_type == 'designer_auditor'
+    end
+    
+    total
+    
+  end
+
 end
