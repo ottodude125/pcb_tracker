2108d6a6ec71d613f4bd6310fc925bec7badc7af
altimont <>
2006-06-29 15:22:01 +0000

    Added the relationship with the user and added the header comment.

--- app/models/audit_comment.rb
+++ app/models/audit_comment.rb
@@ -1,5 +1,18 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: audit_comment.rb
+#
+# This file maintains the state for audit comments.
+#
+# $Id$
+#
+########################################################################
+
 class AuditComment < ActiveRecord::Base
 
   belongs_to :design_check
+  belongs_to :user
 
 end
