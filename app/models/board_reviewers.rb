d5977590cf35de061342519fee65e916f6b119f4
altimont <>
2006-06-29 15:27:56 +0000

    Added the header comment.

--- app/models/board_reviewers.rb
+++ app/models/board_reviewers.rb
@@ -1,2 +1,15 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: board_reviewers.rb
+#
+# This file maintains the state for board reviewers.
+#
+# $Id$
+#
+########################################################################
+
 class BoardReviewers < ActiveRecord::Base
+
 end
