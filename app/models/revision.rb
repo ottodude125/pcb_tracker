3359782fab4a2e024cfa94a627ae34d466f8deee
altimont <>
2006-06-29 15:48:16 +0000

    Added the header comment.
    Added the relationship with designs.

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,3 +1,19 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: revision.rb
+#
+# This file maintains the state for revisions.
+#
+# $Id$
+#
+########################################################################
+
 class Revision < ActiveRecord::Base
+
   has_one :audit
+  
+  has_many :designs
+  
 end
