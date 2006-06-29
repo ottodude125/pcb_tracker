b0123f993634865f7a896ca3a4fd12373a6aa31d
altimont <>
2006-06-29 15:07:35 +0000

    Added header comments.

--- app/helpers/tracker_helper.rb
+++ app/helpers/tracker_helper.rb
@@ -1,6 +1,28 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: tracker_helper.rb
+#
+# This contains the helper methods for tracker views.
+#
+# $Id$
+#
+########################################################################
+#
 module TrackerHelper
 
 
+  ######################################################################
+  #
+  # audit_locked_for_peer
+  #
+  # Description:
+  # Returns true if the audit is not yet available for the peer 
+  # reviewers.
+  #
+  ######################################################################
+  #
   def audit_locked_for_peer(audit)
 
     if audit[:self]
@@ -15,6 +37,16 @@ module TrackerHelper
   end
 
 
+  ######################################################################
+  #
+  # review_locked
+  #
+  # Description:
+  # Returns true if the audit is not yet complete so that the final
+  # audit can not be posted.
+  #
+  ######################################################################
+  #
   def review_locked(design_review)
     if design_review
       audit = design_review.design.audit
@@ -26,12 +58,20 @@ module TrackerHelper
   end
 
 
+  ######################################################################
+  #
+  # get_my_results
+  #
+  # Description:
+  # Returns a list of the user's results for the design review.
+  #
+  ######################################################################
+  #
   def get_my_results(review_id)
 
     my_results = []
 
-    review_results = DesignReviewResult.find_all_by_design_review_id(
-                       review_id)
+    review_results = DesignReviewResult.find_all_by_design_review_id(review_id)
     my_review_results = review_results.delete_if { |r| 
       r.reviewer_id != @session[:user].id 
     }
