51b3e571813ff1b03b861d073d08578535935ec3
altimont <>
2006-07-11 14:59:08 +0000

    Code cleanup

--- app/models/role.rb
+++ app/models/role.rb
@@ -11,7 +11,6 @@
 ########################################################################
 
 class Role < ActiveRecord::Base
-#  has_and_belongs_to_many :permissions
 
   has_many :design_review_results
 
