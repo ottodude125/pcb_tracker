088e2b5a25f7ebfd7f00fa15a57da5ba388073d9
altimont <>
2006-09-26 16:36:37 +0000

    Added the relationship with board_design_entry
    pcb_number() - added
    
    pcb_a_part_numer() - added

--- app/models/prefix.rb
+++ app/models/prefix.rb
@@ -13,7 +13,29 @@
 class Prefix < ActiveRecord::Base
 
   has_many :boards
+  has_many :board_design_entries
 
   validates_uniqueness_of :pcb_mnemonic
+  
+  
+  def pcb_number(number,
+                 revision, 
+                 numeric_revision)
+  
+    self.unloaded_prefix + '-' + number + '-' + revision + numeric_revision.to_s
+  
+  end
+
+
+  def pcb_a_part_number(number,
+                       revision, 
+                       numeric_revision)
+                       
+    rev_table = ('a'..'z').collect
+  
+    self.loaded_prefix + '-' + number + '-' + rev_table.index(revision).to_s + numeric_revision.to_s
+  
+  end
+
 
 end
