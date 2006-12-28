df9954338009b9a39b4e57001d22c260d20bd82c
altimont <>
2006-12-28 21:06:13 +0000

    Cleanup

--- test/functional/board_controller_test.rb
+++ test/functional/board_controller_test.rb
@@ -62,43 +62,15 @@ class BoardControllerTest < Test::Unit::TestCase
     set_non_admin
     post :list
 
-    assert_redirected_to(:controller => 'tracker',
-		                  	 :action     => 'index')
+    assert_redirected_to(:controller => 'tracker', :action => 'index')
     assert_equal(Pcbtr::MESSAGES[:admin_only], flash['notice'])
 
     # Try listing from an Admin account
     # VERIFY: The project list data is retrieved
     set_admin
-    post(:list,
-	       :page => 1)
+    post(:list, :page => 1)
 
     assert_equal(Board.find_all.size, assigns(:boards).size)
-  end
-
-
-  ######################################################################
-  #
-  # test_add
-  #
-  # Description:
-  # This method does the functional testing of the add method
-  # from the Board class
-  #
-  # Parameters:
-  # None
-  #
-  # Return value:
-  # None
-  #
-  ######################################################################
-  #
-  def test_add
-
-    set_admin
-    post :add
-
-    assert_equal(5,  assigns(:platforms).size)
-    assert_equal(14, assigns(:projects).size)
   end
 
 
@@ -149,6 +121,8 @@ class BoardControllerTest < Test::Unit::TestCase
 
     board = Board.find(boards(:la454).id)
     board.prefix_id = prefixes(:la).id
+    
+    assert_equal(0, board.fab_houses.size)
 
     fab_house_selections = {
       '1' => '0',
@@ -167,143 +141,41 @@ class BoardControllerTest < Test::Unit::TestCase
         :fab_house => fab_house_selections)
 
     assert_equal('Board was successfully updated.', flash['notice'])
-    assert_redirected_to(:action => 'edit',
-                         :id     => board.id)
+    assert_redirected_to(:action => 'edit', :id => board.id)
     assert_equal(prefixes(:la).id, board.prefix_id)
-  end
+    
+    board.reload
+    assert_equal(4, board.fab_houses.size)
+    fab_house_selections.each do |fh_id, selected|
+      fab_house = board.fab_houses.detect { |fh| fh.id == fh_id.to_i}
+      if selected == '0'
+        assert_nil(fab_house)
+      else
+        assert_not_nil(fab_house)
+      end
+    end
 
+    fab_house_selections['6'] = '0'
+    fab_house_selections['8'] = '0'
+    get(:update,
+        :board => board.attributes,
+        :board_reviewers => {'8' => '6001', '5' => '6000'},
+        :fab_house => fab_house_selections)
 
-  ######################################################################
-  #
-  # test_create
-  #
-  # Description:
-  # This method does the functional testing of the create method
-  # from the Board class
-  #
-  # Parameters:
-  # None
-  #
-  # Return value:
-  # None
-  #
-  # To Do: Test for duplicate board entry.
-  #
-  ######################################################################
-  #
-  def test_create
+    assert_equal('Board was successfully updated.', flash['notice'])
+    assert_redirected_to(:action => 'edit', :id => board.id)
+    assert_equal(prefixes(:la).id, board.prefix_id)
 
-    assert_equal(7, Board.find_all.size)
-
-    new_board = { 'active'      => '1',
-                  'prefix_id'   => prefixes(:xx).id,
-                  'number'      => '666',
-                  'platform_id' => platforms(:j750).id,
-                  'project_id'  => ''
-    }
-
-    set_admin
-    post(:create,
-         :board => new_board,
-         :board_reviewers => {'8' => '116', '5' => '1331'})
-
-    assert_equal(7, Board.find_all.size)
-    assert_equal("Project can't be blank", flash['notice'])
-    assert_redirected_to :action => 'add'
-
-    new_board = {
-      'active'      => '1',
-      'prefix_id'   => prefixes(:xx).id,
-      'number'      => '666',
-      'platform_id' => '',
-      'project_id'  => projects(:viking).id
-    }
-
-    post(:create,
-         :board => new_board,
-         :board_reviewers => {'8' => '116', '5' => '1331'})
-
-    assert_equal(7, Board.find_all.size)
-    assert_equal("Platform can't be blank", flash['notice'])
-    assert_redirected_to :action => 'add'
-
-    new_board = {
-      'active'      => '1',
-      'prefix_id'   => prefixes(:xx).id,
-      'number'      => '',
-      'platform_id' => platforms(:j750).id,
-      'project_id'  => projects(:viking).id
-    }
-
-    post(:create,
-         :board => new_board,
-         :board_reviewers => {'8' => '116', '5' => '1331'})
-
-    assert_equal(7, Board.find_all.size)
-    assert_equal("Number can't be blank", flash['notice'])
-    assert_redirected_to :action => 'add'
-
-    new_board = {
-      'active'      => '1',
-      'prefix_id'   => '',
-      'number'      => '666',
-      'platform_id' => platforms(:j750).id,
-      'project_id'  => projects(:viking).id
-    }
-
-    post(:create,
-         :board => new_board,
-         :board_reviewers => {'8' => '116', '5' => '1331'})
-
-    assert_equal(7, Board.find_all.size)
-    assert_equal('Prefix can not be blank', flash['notice'])
-    assert_redirected_to :action => 'add'
-
-    new_board = {
-      'active'      => '1',
-      'prefix_id'   => prefixes(:xx).id,
-      'number'      => '666',
-      'platform_id' => platforms(:j750).id,
-      'project_id'  => projects(:viking).id
-    }
-
-    fab_house_selections = {
-      '1' => '0',
-      '2' => '1',
-      '3' => '0',
-      '4' => '1',
-      '5' => '0',
-      '6' => '1',
-      '7' => '0',
-      '8' => '1'}
-
-    set_admin
-    post(:create,
-         :board => new_board,
-         :board_reviewers => {'8' => '116', '5' => '1331'},
-         :fab_house => fab_house_selections)
-
-    assert_equal(8, Board.find_all.size)
-    assert_equal('Board was successfully created', flash['notice'])
-    assert_redirected_to :action => 'list'
-
-    post(:create,
-         :board => new_board,
-         :board_reviewers => {'8' => '116', '5' => '1331'})
-    assert_equal(8, Board.find_all.size)
-    assert_equal("Board xx666 already exists - creation is invalid",
-                 flash['notice'])
-    assert_redirected_to :action => 'add'
-
-    board_bad_number = new_board.dup
-    board_bad_number['number'] = 'dog'
-    post(:create,
-         :board => board_bad_number,
-         :board_reviewers => {'8' => '116', '5' => '1331'})
-
-    assert_equal(8, Board.find_all.size)
-    assert_equal("Number is not a number", flash['notice'])
-    assert_redirected_to :action => 'add'
+    board.reload
+    assert_equal(2, board.fab_houses.size)
+    fab_house_selections.each do |fh_id, selected|
+      fab_house = board.fab_houses.detect { |fh| fh.id == fh_id.to_i}
+      if selected == '0'
+        assert_nil(fab_house)
+      else
+        assert_not_nil(fab_house)
+      end
+    end
 
   end
 
@@ -318,7 +190,7 @@ class BoardControllerTest < Test::Unit::TestCase
   ######################################################################
   #
   def test_filtered_list
-    print '?'
+
   end
   
   
@@ -332,7 +204,23 @@ class BoardControllerTest < Test::Unit::TestCase
   ######################################################################
   #
   def test_show_boards
-    print '?'
+  
+    all_boards = Board.find_all
+    board_list = {}
+    all_boards.each do |board|
+      board_list[board.prefix.pcb_mnemonic] = [] if !board_list[board.prefix.pcb_mnemonic]
+      board_list[board.prefix.pcb_mnemonic] << board
+    end
+    board_list.each_value do |boards|
+      boards = boards.sort_by { |board| board.id }
+    end
+    
+    post(:show_boards)
+    
+    assigns(:boards).each do |prefix_board_list|
+      assert_equal(board_list[prefix_board_list[0]], prefix_board_list[1])
+    end
+    
   end
   
   
@@ -347,7 +235,297 @@ class BoardControllerTest < Test::Unit::TestCase
   ######################################################################
   #
   def test_design_information
-    print '?'
+
+  end
+
+
+  ######################################################################
+  #
+  # test_board_design_search
+  #
+  # Description:
+  # This method does the functional testing of the board_design_search
+  # method
+  #
+  ######################################################################
+  #
+  def test_board_design_search
+  
+    all_boards = { 'mx234' => { :id           => boards(:mx234).id,
+                                :all_designs  => 3,
+                                :post_final   => 0,
+                                :post_release => 0,
+                                :sg_designs   => 0 },
+                   'la453' => { :id           => boards(:la453).id,
+                                :all_designs  => 5,
+                                :post_final   => 1,
+                                :post_release => 1,
+                                :sg_designs   => 4 },
+                   'la454' => { :id           => boards(:la454).id,
+                                :all_designs  => 1,
+                                :post_final   => 0,
+                                :post_release => 0,
+                                :sg_designs   => 0 },
+                   'la455' => { :id           => boards(:la455).id,
+                                :all_designs  => 1,
+                                :post_final   => 0,
+                                :post_release => 0,
+                                :sg_designs   => 1 },
+                   'mx600' => { :id           => boards(:mx600).id,
+                                :all_designs  => 1,
+                                :post_final   => 0,
+                                :post_release => 0,
+                                :sg_designs   => 0 },
+                   'mx700' => { :id           => boards(:mx700).id,
+                                :all_designs  => 1,
+                                :post_final   => 0,
+                                :post_release => 0,
+                                :sg_designs   => 0 },
+                   'mx999' => { :id           => boards(:mx999).id,
+                                :all_designs  => 3,
+                                :post_final   => 0,
+                                :post_release => 0,
+                                :sg_designs   => 0 } }
+  
+    post(:board_design_search,
+         :platform    => { :id    => '' },
+         :project     => { :id    => '' },
+         :user        => { :id    => '' },
+         :review_type => { :phase => 'All'})
+         
+    assert_equal('All Projects',  assigns(:project))
+    assert_equal('All Platforms', assigns(:platform))
+    assert_equal('All Designers', assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(7, board_list.size)
+
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:all_designs], board.designs.size)
+    end 
+
+
+    post(:board_design_search,
+         :platform    => { :id    => '' },
+         :project     => { :id    => '2' },
+         :user        => { :id    => '' },
+         :review_type => { :phase => 'All'})
+         
+    assert_equal('AWG5000',       assigns(:project))
+    assert_equal('All Platforms', assigns(:platform))
+    assert_equal('All Designers', assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(3, board_list.size)
+    
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:all_designs], board.designs.size)
+    end 
+
+
+    post(:board_design_search,
+         :platform    => { :id    => '1' },
+         :project     => { :id    => '' },
+         :user        => { :id    => '' },
+         :review_type => { :phase => 'All'})
+         
+    assert_equal('All Projects',  assigns(:project))
+    assert_equal('Catalyst',      assigns(:platform))
+    assert_equal('All Designers', assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(4, board_list.size)
+    
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:all_designs], board.designs.size)
+    end 
+
+
+    post(:board_design_search,
+         :platform    => { :id    => '2' },
+         :project     => { :id    => '2' },
+         :user        => { :id    => '' },
+         :review_type => { :phase => 'All'})
+         
+    assert_equal('AWG5000',       assigns(:project))
+    assert_equal('FLEX',          assigns(:platform))
+    assert_equal('All Designers', assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(3, board_list.size)
+    
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:all_designs], board.designs.size)
+    end 
+
+
+    post(:board_design_search,
+         :platform    => { :id    => '' },
+         :project     => { :id    => '' },
+         :user        => { :id    => users(:scott_g).id },
+         :review_type => { :phase => 'All'})
+         
+    assert_equal('All Projects',  assigns(:project))
+    assert_equal('All Platforms', assigns(:platform))
+    assert_equal('Scott Glover',  assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(7, board_list.size)
+    
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:sg_designs], board.designs.size)
+    end 
+
+
+    post(:board_design_search,
+         :platform    => { :id    => '' },
+         :project     => { :id    => '' },
+         :user        => { :id    => '' },
+         :review_type => { :phase => 'Final'})
+         
+    assert_equal('All Projects',  assigns(:project))
+    assert_equal('All Platforms', assigns(:platform))
+    assert_equal('All Designers', assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(7, board_list.size)
+    
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:post_final], board.designs.size)
+    end 
+
+
+    post(:board_design_search,
+         :platform    => { :id    => '' },
+         :project     => { :id    => '' },
+         :user        => { :id    => '' },
+         :review_type => { :phase => 'Release'})
+         
+    assert_equal('All Projects',  assigns(:project))
+    assert_equal('All Platforms', assigns(:platform))
+    assert_equal('All Designers', assigns(:designer))
+    
+    board_list = assigns(:board_list)
+    assert_equal(7, board_list.size)
+    
+    board_list.each do |board|
+      expected_brd = all_boards.detect { |k,v| k == board.name}
+      assert_not_nil(expected_brd)
+
+      expected_board = Board.find(expected_brd[1][:id])
+      assert_equal(expected_brd[1][:post_release], board.designs.size)
+    end 
+
+  end
+
+
+  ######################################################################
+  #
+  # test_design_information
+  #
+  # Description:
+  # This method does the functional testing of the design_information
+  # method
+  #
+  ######################################################################
+  #
+  def test_design_information
+  
+    post(:design_information, :board => { :name => 'junk' })
+    
+    assert_redirected_to('action' => 'show_boards')
+    assert_equal('Please provide a board number', flash['notice'])
+  
+    post(:design_information, :board => { :name => 'la453' })
+    
+    assert_response(:success)
+    board = assigns(:board)
+    assert_equal('la453', board.name)
+  
+    mx999 = boards(:mx999)
+    post(:design_information, :board_id => mx999.id)
+    
+    assert_response(:success)
+    board = assigns(:board)
+    assert_equal(mx999.name, board.name)
+  
+  end
+  
+  
+  ######################################################################
+  #
+  # test_seach_options
+  #
+  # Description:
+  # This method does the functional testing of the seach_options
+  # method
+  #
+  ######################################################################
+  #
+  def test_search_options
+  
+    platform_ids = Platform.find_all.collect { |p| p.id }
+    project_ids  = Project.find_all.collect  { |p| p.id }
+    users        = User.find_all
+
+    designers = User.find_all.delete_if do |u| 
+      !u.roles.detect { |r| r.name == "Designer" } 
+    end
+    designers = designers.sort_by { |d| d.id }
+    
+    login_list = [nil,             # Nobody
+                  { :id => users(:scott_g).id, :role => 'Designer' }, 
+                  { :id => users(:pat_a).id,   :role => 'Product Support' }]
+                  
+    login_list.each do |login|
+    
+      set_user(login[:id], login[:role]) if login
+    
+      # Call without being logged in and verify the data
+      post(:search_options)
+
+      if login && login[:role] == 'Designer'
+        assert_equal(login[:id], assigns(:designer).id)
+      else
+        assert_nil(assigns(:designer))
+      end
+    
+      platforms = assigns(:platforms)
+      assert_equal(platform_ids.size, platforms.size)
+      platform_ids.each { |id| assert_not_nil(platforms.detect { |p| p.id == id }) }
+    
+      projects  = assigns(:projects)
+      assert_equal(project_ids.size, projects.size)
+      project_ids.each { |id| assert_not_nil(projects.detect { |p| p.id == id }) }
+      
+      assert_equal(designers, assigns(:designers).sort_by  { |d| d.id })
+      
+    end
+
   end
   
   
