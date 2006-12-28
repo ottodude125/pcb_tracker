bea3dd5eee22450056d7b33292abe96c7c1fa83a
altimont <>
2006-12-28 18:54:59 +0000

    Added the get_all_active() method.
    code cleanup.

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,5 +18,33 @@ class Project < ActiveRecord::Base
 
   validates_uniqueness_of :name
   validates_presence_of :name
+  
+  
+  ##############################################################################
+  #
+  # Class Methods
+  # 
+  ##############################################################################
+
+  
+  ######################################################################
+  #
+  # get_all_active
+  #
+  # Description:
+  # This method returns a list of the active projects
+  #
+  # Parameters:
+  # sort - specifies the field(s) and sort order
+  #
+  # Return value:
+  # An array of active project records
+  #
+  ######################################################################
+  #
+  def Project.get_all_active(sort = 'name ASC')
+    Project.find_all_by_active(1, sort)
+  end
+
 
 end
