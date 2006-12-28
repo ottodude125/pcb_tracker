ecc795d504fb67ee298fbfc32ddddef5184b4dee
altimont <>
2006-12-28 18:53:02 +0000

    Added get_all_active()
    Code cleanup.

--- app/models/prefix.rb
+++ app/models/prefix.rb
@@ -17,19 +17,87 @@ class Prefix < ActiveRecord::Base
 
   validates_uniqueness_of :pcb_mnemonic
   
+  ##############################################################################
+  #
+  # Class Methods
+  # 
+  ##############################################################################
+
   
+  ######################################################################
+  #
+  # get_all_active
+  #
+  # Description:
+  # This method returns a list of the active prefixes
+  #
+  # Parameters:
+  # sort - specifies the field(s) and sort order
+  #
+  # Return value:
+  # An array of active prefix records
+  #
+  ######################################################################
+  #
+  def Prefix.get_all_active(sort = 'pcb_mnemonic ASC')
+    Prefix.find_all_by_active(1, sort)
+  end
+  
+  
+  ##############################################################################
+  #
+  # Instance Methods
+  # 
+  ##############################################################################
+
+  
+  ######################################################################
+  #
+  # pcb_number
+  #
+  # Description:
+  # This method returns the pcb number associated with the prefix record.
+  #
+  # Parameters:
+  # number           - the design's board number
+  # revision         - the design's alphabetic revision
+  # numeric_revision - the design's numeric revision
+  #
+  # Return value:
+  # A string representing the pcb_number.
+  #
+  ######################################################################
+  #
   def pcb_number(number,
                  revision, 
                  numeric_revision)
-  
+                 
     self.unloaded_prefix + '-' + number + '-' + revision + numeric_revision.to_s
-  
+    
   end
 
 
+  ######################################################################
+  #
+  # pcb_a_part_number
+  #
+  # Description:
+  # This method returns the pcba part number associated with the prefix
+  # record.
+  #
+  # Parameters:
+  # number           - the design's board number
+  # revision         - the design's alphabetic revision
+  # numeric_revision - the design's numeric revision
+  #
+  # Return value:
+  # A string representing the pcba part number.
+  #
+  ######################################################################
+  #
   def pcb_a_part_number(number,
-                       revision, 
-                       numeric_revision)
+                        revision, 
+                        numeric_revision)
                        
     rev_table = ('a'..'z').collect
   
