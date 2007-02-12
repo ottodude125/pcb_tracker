59fea1bf87506e73d9fb7384a354d7b8449decfa
altimont <>
2007-02-12 19:55:37 +0000

    Added test_next()
    DSR 7177

--- test/unit/review_type_test.rb
+++ test/unit/review_type_test.rb
@@ -1,3 +1,16 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: review_type_test.rb
+#
+# This file contains the unit tests for the review type model
+#
+# Revision History:
+#   $Id$
+#
+########################################################################
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ReviewTypeTest < Test::Unit::TestCase
@@ -7,8 +20,23 @@ class ReviewTypeTest < Test::Unit::TestCase
     @review_type = ReviewType.find(1)
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of ReviewType,  @review_type
+  ##############################################################################
+  def test_next
+  
+    pre_art   = review_types(:pre_artwork)
+    
+    next_review_type = pre_art.next
+    assert_equal(review_types(:placement).id, next_review_type.id)
+    next_review_type = next_review_type.next
+    assert_equal(review_types(:routing).id,   next_review_type.id)
+    next_review_type = next_review_type.next
+    assert_equal(review_types(:final).id,     next_review_type.id)
+    next_review_type = next_review_type.next
+    assert_equal(review_types(:release).id,   next_review_type.id)
+    next_review_type = next_review_type.next
+    assert_nil(next_review_type)
+
   end
+  
+  
 end
