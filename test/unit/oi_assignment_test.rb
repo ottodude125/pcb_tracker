f3759cbf1e99e8f03b15bacb31b9322f16c69d2c
altimont <>
2007-02-12 20:07:48 +0000

    Added test_complexity_list(), test_complexity_name(), and test_complexity_id()
    DSR 7133

--- test/unit/oi_assignment_test.rb
+++ test/unit/oi_assignment_test.rb
@@ -17,8 +17,34 @@ require File.dirname(__FILE__) + '/../test_helper'
 class OiAssignmentTest < Test::Unit::TestCase
   fixtures :oi_assignments
 
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of OiAssignment, oi_assignments(:first)
+  ######################################################################
+  def test_complexity_list
+  
+    expected_complexity_list = [ ['High', 1], ['Medium', 2], ['Low', 3] ]
+    assert_equal(expected_complexity_list, OiAssignment.complexity_list)
+    
   end
+
+
+  ######################################################################
+  def test_complexity_name
+  
+    assert_equal('High',      OiAssignment.complexity_name(1))
+    assert_equal('Medium',    OiAssignment.complexity_name(2))
+    assert_equal('Low',       OiAssignment.complexity_name(3))
+    assert_equal('Undefined', OiAssignment.complexity_name(33))
+    
+  end
+
+
+  ######################################################################
+  def test_complexity_id
+  
+    assert_equal(1, OiAssignment.complexity_id('High'))
+    assert_equal(2, OiAssignment.complexity_id('Medium'))
+    assert_equal(3, OiAssignment.complexity_id('Low'))
+    
+  end
+
+
 end
