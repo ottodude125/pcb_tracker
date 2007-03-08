2c49a0157f98f3602d231c222ffad3d2ec1fb590
altimont <>
2007-03-08 19:53:20 +0000

    Initial version
    DSR 6904

---
+++ test/unit/ftp_notification_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class FtpNotificatonTest < Test::Unit::TestCase
+  fixtures :ftp_notifications
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
