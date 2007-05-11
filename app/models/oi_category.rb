eba0b6ff0772d3930f6df429f5b8fcaa8e1aa4cd
altimont <>
2007-05-11 20:01:30 +0000

    Modified the relationship with oi_category_sections to order oi_category_sections by id.
    Added list(), other_category_section_id(), and other?().
    
    DSR 7271

--- app/models/oi_category.rb
+++ app/models/oi_category.rb
@@ -12,6 +12,74 @@
 
 class OiCategory < ActiveRecord::Base
 
-  has_many :oi_category_sections
+
+  has_many(:oi_category_sections, 
+           :order => :id)
+
+           
+  ##############################################################################
+  #
+  # Class Methods
+  # 
+  ##############################################################################
+
+
+  ######################################################################
+  #
+  # list
+  #
+  # Description:
+  # This method returns a list of categories ordered by the ID.
+  #
+  # Parameters:
+  # None
+  #
+  ######################################################################
+  #
+  def self.list
+    self.find(:all, :order => :id)
+  end
+
+
+  ######################################################################
+  #
+  # other_category_section_id
+  #
+  # Description:
+  # This method returns the category section id of "Other"
+  #
+  # Parameters:
+  # None
+  #
+  ######################################################################
+  #
+  def self.other_category_section_id
+    self.find_by_name("Other").oi_category_sections[0].id
+  end
+
+
+  ##############################################################################
+  #
+  # Instance Methods
+  # 
+  ##############################################################################
+
+
+  ######################################################################
+  #
+  # other?
+  #
+  # Description:
+  # This method returns True if the category is "Other"
+  #
+  # Parameters:
+  # None
+  #
+  ######################################################################
+  #
+  def other?
+    self.name == 'Other'
+  end
+
 
 end
