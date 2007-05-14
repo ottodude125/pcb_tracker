2808534fb7089fd8dec0a82df2e87dd86f58a714
altimont <>
2007-05-14 16:13:26 +0000

    Addressed deprecation issues and modified tests.

--- test/unit/oi_category_test.rb
+++ test/unit/oi_category_test.rb
@@ -30,7 +30,7 @@ class OiCategoryTest < Test::Unit::TestCase
   ######################################################################s
   def test_categories
   
-    oi_category_list = OiCategory.find_all.sort_by{ |oi| oi.id }
+    oi_category_list = OiCategory.find(:all).sort_by{ |oi| oi.id }
     
     assert_equal(7, oi_category_list.size)
     assert_equal('Board Preparation',   oi_category_list[0].name)
@@ -40,6 +40,13 @@ class OiCategoryTest < Test::Unit::TestCase
     assert_equal('Nomenclature',        oi_category_list[4].name)
     assert_equal('Assembly Drawing',    oi_category_list[5].name)
     assert_equal('Other',               oi_category_list[6].name)
+    assert(!oi_category_list[0].other?)
+    assert(!oi_category_list[1].other?)
+    assert(!oi_category_list[2].other?)
+    assert(!oi_category_list[3].other?)
+    assert(!oi_category_list[4].other?)
+    assert(!oi_category_list[5].other?)
+    assert(oi_category_list[6].other?)
 
     assert_equal(1, oi_category_list[0].id)
     assert_equal(2, oi_category_list[1].id)
@@ -48,6 +55,11 @@ class OiCategoryTest < Test::Unit::TestCase
     assert_equal(5, oi_category_list[4].id)
     assert_equal(6, oi_category_list[5].id)
     assert_equal(7, oi_category_list[6].id)
+    
+    assert_equal(oi_category_list, OiCategory.list)
+    assert_equal(32,               OiCategory.other_category_section_id)
 
   end
+  
+  
 end
