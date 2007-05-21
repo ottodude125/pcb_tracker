66f8b169ff78885edabe44ff208ad4367844e84e
altimont <>
2007-05-21 17:37:46 +0000

    Addressed deprecation issues

--- app/controllers/division_controller.rb
+++ app/controllers/division_controller.rb
@@ -24,7 +24,7 @@ class DivisionController < ApplicationController
   # display.  The list is paginated and is limited to the number 
   # passed to the ":per_page" argument.
   #
-  # Parameters from @params
+  # Parameters from params
   # None
   #
   # Return value:
@@ -49,7 +49,7 @@ class DivisionController < ApplicationController
   # Description:
   # This method retrieves the division from the database for display.
   #
-  # Parameters from @params
+  # Parameters from params
   # ['id'] - Used to identify the division to be retrieved.
   #
   # Return value:
@@ -58,7 +58,7 @@ class DivisionController < ApplicationController
   ######################################################################
   #
   def edit 
-    @division = Division.find(@params['id'])
+    @division = Division.find(params[:id])
   end
 
 
@@ -70,7 +70,7 @@ class DivisionController < ApplicationController
   # This method uses information passed back from the edit screen to
   # update the database.
   #
-  # Parameters from @params
+  # Parameters from params
   # ['division'] - Used to identify the division to be updated.
   #
   # Return value:
@@ -79,16 +79,16 @@ class DivisionController < ApplicationController
   ######################################################################
   #
   def update
-    @division = Division.find(@params['division']['id'])
+    @division = Division.find(params[:division][:id])
 
-    if @division.update_attributes(@params['division'])
+    if @division.update_attributes(params[:division])
       flash['notice'] = "Division #{@division.name} was successfully updated."
       redirect_to :action => 'edit', 
-                  :id     => @params["division"]["id"]
+                  :id     => params[:division][:id]
     else
       flash['notice'] = @division.errors.full_messages.pop
       redirect_to :action => 'edit', 
-                  :id     => @params["division"]["id"]
+                  :id     => params[:division][:id]
     end
 
   end
@@ -102,7 +102,7 @@ class DivisionController < ApplicationController
   # This method uses the information passed back from the user
   # to create a new division in the database
   #
-  # Parameters from @params
+  # Parameters from params
   # ['new_division'] - the information to be stored for the new division.
   #
   # Return value:
@@ -112,7 +112,7 @@ class DivisionController < ApplicationController
   #
   def create
 
-    @division = Division.create(@params['new_division'])
+    @division = Division.create(params[:new_division])
 
     if @division.errors.empty?
       flash['notice'] = "Division #{@division.name} added"
