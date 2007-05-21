792dc13648b5dc98d6bdbbe27033973ae64b5954
altimont <>
2007-05-21 18:50:14 +0000

    Added self_auditor_checked?(), peer_auditor_checked?(), and comment_required?()

--- app/models/design_check.rb
+++ app/models/design_check.rb
@@ -60,4 +60,73 @@ class DesignCheck < ActiveRecord::Base
     end
   end
   
+
+  ######################################################################
+  #
+  # self_auditor_checked?
+  #
+  # Description:
+  # Indicates if the self audit design check is complete
+  #
+  # Return value:
+  # TRUE if the self auditor has completed the design check, FALSE 
+  # otherwise.
+  #
+  ######################################################################
+  #
+  def self_auditor_checked?
+    self.designer_result != 'None'
+  end
+  
+
+  ######################################################################
+  #
+  # peer_auditor_checked?
+  #
+  # Description:
+  # Indicates if the peer audit design check is complete
+  #
+  # Return value:
+  # TRUE if the peer auditor has completed the design check, FALSE 
+  # otherwise.
+  #
+  ######################################################################
+  #
+  def peer_auditor_checked?
+    !(self.auditor_result == 'None' || self.auditor_result == 'Comment')
+  end
+  
+  
+  ######################################################################
+  #
+  # comment_required?
+  #
+  # Description:
+  # Indicates if a comment is recquired for the update.
+  # 
+  # Parameters:
+  # designer_result - the result entered by the designer
+  # auditor_result  - the result entered by the auditor
+  #
+  # Return value:
+  # TRUE if the peer auditor has completed the design check, FALSE 
+  # otherwise.
+  #
+  ######################################################################
+  #
+  def comment_required?(designer_result, auditor_result)
+
+    # Checking the check for the check type is overkill.
+    case 
+      when designer_result == 'No'
+        self.check.yes_no?
+      when designer_result == 'Waived'
+        self.check.designer_only? || self.check.designer_auditor?
+      when auditor_result  == 'Waived'  || auditor_result == 'Comment'
+        self.check.designer_auditor?
+    end
+
+  end
+  
+  
 end
