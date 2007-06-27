65f92144925ebdf64bf1b0ef48edffe04203cf4f
altimont <>
2007-06-27 19:40:31 +0000

    Cleanup
    Defined the REVIEW_COMPLETE and POSITIVE_RESULT constants.
    
    Modified complete?() to use the REVIEW_COMPLETE constant.
    
    Added positive_response?() method.

--- app/models/design_review_result.rb
+++ app/models/design_review_result.rb
@@ -15,10 +15,12 @@ class DesignReviewResult < ActiveRecord::Base
   belongs_to :design_review
   belongs_to :role
   
-  
   APPROVED = 'APPROVED'
   REJECTED = 'REJECTED'
   WAIVED   = 'WAIVED'
+ 
+  REVIEW_COMPLETE = [APPROVED, REJECTED, WAIVED]
+  POSITIVE_RESULT = [APPROVED, WAIVED]
 
   ######################################################################
   #
@@ -50,8 +52,26 @@ class DesignReviewResult < ActiveRecord::Base
   ######################################################################
   #
   def complete?
-    self.result == APPROVED || self.result == WAIVED || self.result == REJECTED
+    REVIEW_COMPLETE.include?(self.result)
   end
+
   
+  ######################################################################
+  #
+  # positive_response?
+  #
+  # Description:
+  # Indicates that the review result has been processed by the reviewer
+  # and a positive response has been entered.
+  #
+  # Return value:
+  # TRUE if the review result is either APPROVED or WAIVED, otherwise FALSE
+  #
+  ######################################################################
+  #
+  def positive_response?
+    POSITIVE_RESULT.include?(self.result)
+  end
+
   
 end
