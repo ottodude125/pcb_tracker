95d16481b6edd05353df65130cd1aff30bea9047
altimont <>
2007-06-28 13:52:47 +0000

    Added accessor methods

--- app/models/review_type.rb
+++ app/models/review_type.rb
@@ -34,11 +34,175 @@ class ReviewType < ActiveRecord::Base
   # 
   ##############################################################################
 
+
+  ######################################################################
+  #
+  # get_active_review_types
+  #
+  # Description:
+  # This method retrieves all of the active review type records
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A list of active review types ordered by the sort_order attribute
+  #
+  ######################################################################
+  #
+  def self.get_active_review_types
+    self.find(:all, :conditions => 'active=1', :order => 'sort_order')
+  end
   
+  
+  ######################################################################
+  #
+  # get_review_types
+  #
+  # Description:
+  # This method retrieves all of the review type records
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A list of review types ordered by the sort_order attribute
+  #
+  ######################################################################
+  #
+  def self.get_review_types
+    self.find(:all, :order => 'sort_order')
+  end
+  
+  
+  ######################################################################
+  #
+  # get_pre_artwork
+  #
+  # Description:
+  # This method retrieves the Pre-Artwork Review Type record
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A review type record
+  #
+  ######################################################################
+  #
+  def self.get_pre_artwork
+    self.find_by_name('Pre-Artwork')
+  end
+  
+  
+  ######################################################################
+  #
+  # get_placement
+  #
+  # Description:
+  # This method retrieves the Placement Review Type record
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A review type record
+  #
+  ######################################################################
+  #
+  def self.get_placement
+    self.find_by_name('Placement')
+  end
+  
+  
+  ######################################################################
+  #
+  # get_routing
+  #
+  # Description:
+  # This method retrieves the Routing Review Type record
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A review type record
+  #
+  ######################################################################
+  #
+  def self.get_routing
+    self.find_by_name('Routing')
+  end
+  
+
+  ######################################################################
+  #
+  # get_final
+  #
+  # Description:
+  # This method retrieves the Final Review Type record
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A review type record
+  #
+  ######################################################################
+  #
+  def self.get_final
+    self.find_by_name('Final')
+  end
+  
+  
+  ######################################################################
+  #
+  # get_release
+  #
+  # Description:
+  # This method retrieves the Release Review Type record
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A review type record
+  #
+  ######################################################################
+  #
+  def self.get_release
+    self.find_by_name('Release')
+  end
+  
+  
+  ##############################################################################
+  #
+  # Instance Methods
+  # 
+  ##############################################################################
+
+  
+  ######################################################################
+  #
+  # next
+  #
+  # Description:
+  # This method retrieves the review type that follows this instance
+  # of review types based on the sort_order attribute.
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A review type record if this instance is not the last one in the
+  # list.  Otherwise, Nil is returned.
+  #
+  ######################################################################
+  #
   def next
     ReviewType.find(:first,
                     :conditions => "active = 1 AND sort_order > '#{self.sort_order}'",
-                    :order      => "sort_order ASC")
+                    :order      => "sort_order")
   end
 
 
