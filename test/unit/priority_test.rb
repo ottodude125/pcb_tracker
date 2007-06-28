a73e6d1aa8de9daf0cfebf4181feca9379c99a79
altimont <>
2007-06-28 14:27:40 +0000

    Added header comments.
    Added code to test the accessor methods.

--- test/unit/priority_test.rb
+++ test/unit/priority_test.rb
@@ -1,14 +1,38 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: priority_test.rb
+#
+# This file contains the unit tests for the priority model
+#
+# Revision History:
+#   $Id$
+#
+########################################################################
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PriorityTest < Test::Unit::TestCase
   fixtures :priorities
 
-  def setup
-    @priority = Priority.find(1)
-  end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of Priority,  @priority
+  def setup
+    @high   = priorities(:high)
+    @medium = priorities(:medium)
+    @low    = priorities(:low)
   end
+  
+
+  ######################################################################
+  def test_get
+    
+    expected = [ @high, @medium, @low ]
+    
+    Priority.get_priorities.each_with_index do |priority, i|
+      assert_equal(expected[i], priority)
+    end
+    
+  end
+  
 end
