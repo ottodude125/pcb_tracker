596d83ff9c1cb6dc7f7d4995fc4e01678ae03095
altimont <>
2007-07-02 17:28:06 +0000

    Added the get_revisions() class method.

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -16,5 +16,33 @@ class Revision < ActiveRecord::Base
   
   has_many :board_design_entries
   has_many :designs
+
   
+  ##############################################################################
+  #
+  # Class Methods
+  # 
+  ##############################################################################
+
+  
+  ######################################################################
+  #
+  # get_revisions
+  #
+  # Description:
+  # This method returns a list of revisions
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A list of all revision records in the database.
+  #
+  ######################################################################
+  #
+  def self.get_revisions
+    self.find(:all, :order => 'name')
+  end
+
+
 end
