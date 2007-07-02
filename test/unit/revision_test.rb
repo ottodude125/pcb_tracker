bf9546fc2a9bd228c7c29cdd76e5d2f6f4e3009a
altimont <>
2007-07-02 19:01:32 +0000

    Added header comments.
    Test Revision accessor methods.

--- test/unit/revision_test.rb
+++ test/unit/revision_test.rb
@@ -1,14 +1,40 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: revision_test.rb
+#
+# This file contains the unit tests for the revision model
+#
+# Revision History:
+#   $Id$
+#
+########################################################################
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class RevisionTest < Test::Unit::TestCase
   fixtures :revisions
 
   def setup
-    @revision = Revision.find(1)
+    @revisions = [ revisions(:rev_a),   revisions(:rev_b),
+                   revisions(:rev_c),   revisions(:rev_d),
+                   revisions(:rev_e),   revisions(:rev_f),
+                   revisions(:rev_g) ]
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of Revision,  @revision
+  ######################################################################
+  def test_get
+  
+    revision_list = Revision.get_revisions
+    
+    assert_equal(@revisions, revision_list)
+    
+    revision_list.each_with_index do |rev, i|
+      assert_equal(@revisions[i], rev)
+    end
+    
   end
+  
+  
 end
