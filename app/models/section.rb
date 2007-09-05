8ef97133b522f21d61113cd65cb2c913bb27f73c
altimont <>
2007-09-05 18:23:46 +0000

    Added the acts_as_list statement. Removed the relationship with checks. Added insert(), remove(), and locked?() to encapsulate business logic.

--- app/models/section.rb
+++ app/models/section.rb
@@ -12,11 +12,12 @@
 
 class Section < ActiveRecord::Base
 
-  belongs_to :checklist
+  belongs_to(:checklist)
+  acts_as_list(:scope => :checklist)
   
-  has_many   :audit_teammates
-  has_many   :checks
-  has_many(:subsections,         :order => 'sort_order ASC')  
+  has_many(:audit_teammates)
+
+  has_many(:subsections, :order => :position)  
 
 
   ######################################################################
@@ -34,12 +35,89 @@ class Section < ActiveRecord::Base
   def designer_auditor_checks
     
     total = 0
-    
-    for subsection in self.subsections
-      total += subsection.designer_auditor_checks
-    end
+    self.subsections.each { |ss| total += ss.designer_auditor_checks }
     
     total
     
   end
+  
+  
+  ######################################################################
+  #
+  # insert
+  #
+  # Description:
+  # Inserts the object into the checklist's list of sections.
+  #
+  # Parameters:
+  # checklist_id - the checklist identifier
+  # position     - The position in the list that the section is to be 
+  #                inserted at.
+  #
+  # Return value:
+  # self.errors - if the checklist could not be stored in the database then
+  #               this structure will contain errors.
+  #
+  ######################################################################
+  #
+  def insert(checklist_id, position)
+
+    self.checklist_id = checklist_id
+    self.create
+    
+    self.checklist.sections.last.insert_at(position) if self.errors.empty?
+
+  end
+
+
+  ######################################################################
+  #
+  # remove
+  #
+  # Description:
+  # Removes the section and all of its children (subsections).
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # TRUE if the section and all of its subsections were removed, FALSE
+  # otherwise.
+  #
+  ######################################################################
+  #
+  def remove
+
+   removed = true
+   self.subsections.each do |subsection|
+     removed = subsection.remove
+     break if !removed
+   end
+
+   return removed && self.destroy
+    
+  end
+
+  
+  ######################################################################
+  #
+  # locked?
+  #
+  # Description:
+  # Reports whether or not the object is available for modification and
+  # deletion.
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # TRUE if the object can not be deleted or modified, otherwise FALSE.
+  #
+  ######################################################################
+  #
+  def locked?
+    self.checklist.locked?
+  end
+
+
 end
