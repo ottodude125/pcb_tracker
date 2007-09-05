506e786a2295e166631f31f3e2eb0f77b0231b3a
altimont <>
2007-09-05 16:07:31 +0000

    Removed validations that supported the design name.  (DSR 7438)

--- app/models/board.rb
+++ app/models/board.rb
@@ -24,30 +24,9 @@ class Board < ActiveRecord::Base
   has_and_belongs_to_many :fab_houses
   has_and_belongs_to_many :users
 
-  validates_presence_of :number
   validates_presence_of :platform_id
-  validates_presence_of :prefix_id
   validates_presence_of :project_id
 
-  validates_numericality_of :number
-
-
-  ######################################################################
-  #
-  # validate_on_create
-  #
-  # Description:
-  # This method prevents duplicate boards from being created
-  #
-  ######################################################################
-  #
-  def validate_on_create
-    new_board = Board.find_by_number_and_prefix_id(number, prefix_id)
-    if new_board
-      errors.add("Board #{new_board.name} already exists - creation")
-    end
-  end
-  
 
   ##############################################################################
   #
