485c6f19d3249b9041ccc82f836f69d37aff2e81
altimont <>
2007-09-25 18:38:55 +0000

    Added get_active-design_directories() to move business logic into the model.

--- app/models/design_directory.rb
+++ app/models/design_directory.rb
@@ -17,4 +17,32 @@ class DesignDirectory < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
 
+
+  ##############################################################################
+  #
+  # Class Methods
+  # 
+  ##############################################################################
+
+  
+  ######################################################################
+  #
+  # get_active_design_directories
+  #
+  # Description:
+  # This method returns a list of the active design directory records
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # A list of active design directory records
+  #
+  ######################################################################
+  #
+  def self.get_active_design_directories
+    self.find(:all, :conditions => 'active=1', :order => 'name')
+  end
+
+
 end
