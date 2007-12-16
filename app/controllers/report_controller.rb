4441d747ce103cee0c1c49849c984a42b94d8358
altimont <>
2007-12-16 02:39:04 +0000

    Modified to support graphs in the report card rollup. Modified report_card_rollup(). Added create_graph()

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -10,6 +10,10 @@
 #
 ########################################################################
 
+
+require 'gruff'
+
+
 class ReportController < ApplicationController
 
 
@@ -58,81 +62,79 @@ class ReportController < ApplicationController
   # category      - If 0 then all categories should be included in the
   #                 results.  Otherwise, it is the identifier of the 
   #                 category to include in the results.
-  # start_date    - Contains the start date in the date range for the
-  #                 query condition
-  # end_date      - Contains the end date in the date range for the
-  #                 query condition
+  # date          - Indicates the quarter that the user wants to gather
+  #                 reports for
+  # end_date      - Indicates the year that the user wants to gather
+  #                 reports for
+  # download      - a check box that indicates that the user want to
+  #                 download the graph
   #
   ######################################################################
   #
   def report_card_rollup
   
-    @ter_designers = []
-    @lcr_designers = []
-    Role.find_by_name("Designer").users.sort_by { |u| u.last_name}.each do |designer|
-      if designer.employee?
-        @ter_designers << designer
-      else
-        @lcr_designers << designer
-      end
-    end
+    @lcr_designers = Role.lcr_designers
     
-    @categories = OiCategory.find_all.sort_by { |c| c.id }
-
-
-    @lead_designer_id = params[:lead_designer] ? params[:lead_designer][:id].to_i : 0
     @team_member_id   = params[:team_member]   ? params[:team_member][:id].to_i   : 0
-    @category_id      = params[:category]      ? params[:category][:id].to_i      : 0
-    
-    if !params[:start_date]
-      @start_date = start_of_quarter()
-    else
-      date = params[:start_date]
-      @start_date = Time.local(date[:year], date[:month], date[:day]).to_date
-    end
-    
-    if !params[:end_date]
-      @end_date   = Date.today
-    else
-      date = params[:end_date]
-      @end_date = Time.local(date[:year], date[:month], date[:day]).to_date
-    end
-    
-    end_date = (@end_date.to_time + 1.day).to_date
-    query_conditions = "created_on >= '#{@start_date}'" +
-                       " and created_on <= '#{end_date}'"
-    if @lead_designer_id > 0
-      query_conditions += " and user_id = '#{@lead_designer_id}'"
-    end
-    
-    if @end_date < @start_date
-      flash['notice'] = "Warning: the end date preceeds the start date - no reports retrieved"
-    else
-      flash['notice'] = nil
-    end
+    @quarter = !params[:quarter] ? current_quarter()  : params[:quarter][:number].to_i
+    @year    = !params[:date]    ? Time.now.year      : params[:date][:year].to_i
 
-
-    report_cards = OiAssignmentReport.find_all(query_conditions)
+    start_month = (@quarter - 1) * 3 + 1
+    end_month   = start_month < 10 ? start_month + 3 : 1
+    end_year    = start_month < 10 ? @year           : @year + 1
+    
+    start_date = Date.new(@year,    start_month, 1)
+    end_date   = Date.new(end_year, end_month,   1)
+    conditions = "created_on >= '#{start_date}' and created_on < '#{end_date}'"
+    @report_cards = OiAssignmentReport.find(:all,
+                                            :conditions => conditions,
+                                            :order      => 'created_on')
+    @report_cards.delete_if { |rc| !rc.oi_assignment.complete? }
     
     if @team_member_id > 0
-      report_cards.delete_if { |rc| rc.oi_assignment.user_id != @team_member_id }
-    end
-
-    if @category_id > 0
-      report_cards.delete_if do |rc| 
-        rc.oi_assignment.oi_instruction.oi_category_section.oi_category_id != @category_id
-      end
+      @report_cards.delete_if { |rc| rc.oi_assignment.user_id != @team_member_id }
     end
     
-    @report_cards = report_cards.sort_by { |rc| rc.created_on }
+    category_count = OiCategory.count
+    score_summary = { 'High'   => { :count => Array.new(category_count, 0), 
+                                    :total => Array.new(category_count, 0) },
+                      'Medium' => { :count => Array.new(category_count, 0), 
+                                    :total => Array.new(category_count, 0) },
+                      'Low'    => { :count => Array.new(category_count, 0), 
+                                    :total => Array.new(category_count, 0) } }
 
     @total_score = 0
-    @score_distribution = {}
-    @score_distribution.default= 0
     @report_cards.each do |report_card| 
-      @total_score                           += report_card.score
-      @score_distribution[report_card.score] += 1 
+      @total_score += report_card.score
+      
+      i = report_card.oi_assignment.oi_instruction.oi_category_section.oi_category.id - 1
+      
+      summary = score_summary[report_card.oi_assignment.complexity_name]
+      summary[:count][i] += 1
+      summary[:total][i] += report_card.score
+
     end
+    
+    ##################################################
+    score_summary.each do |complexity, data|
+      logger.info("#### COMPLEXITY: #{complexity}")
+      data.each do |label, arr|
+        logger.info(" ---- #{label}")
+        arr.each { |av| logger.info("      #{av}") }
+      end
+    end
+    ##################################################
+    
+    download = params[:download] && params[:download][:yes] == '1'
+    
+    designer = @lcr_designers.detect { |d| d.id == @team_member_id }
+    idc_designer = designer ? designer.name : ''
+    create_graph(score_summary,
+                 @quarter,
+                 @year,
+                 idc_designer,
+                 download)
+    
   end
   
   
@@ -195,6 +197,75 @@ class ReportController < ApplicationController
   rescue
     flash['notice'] = "Error: design review id not found in the database"
     redirect_to(:controller => 'tracker', :action => 'index') 
+  end
+  
+  
+private
+
+  def create_graph(score_summary,
+                   quarter, 
+                   year,
+                   idc_designer,
+                   download)
+  
+    title = 'LCR Process Step Evaluation'
+    date  = "Q#{quarter}-#{year}"
+    graph = Gruff::Bar.new
+
+    if idc_designer == ''
+      graph.title = "#{date} #{title} Roll Up"
+    else
+      graph.title = "#{date} #{title} - #{idc_designer}"
+    end
+
+    # Adjust the font according to the length of the title
+    graph.title_font_size  = graph.title.size < 50 ? 26 : 22
+
+    #graph.theme_37signals
+    
+    graph.minimum_value    = 0
+    graph.maximum_value    = 100
+    graph.marker_count     = 10
+    
+    graph.sort             = false
+    graph.y_axis_label     = 'Percentage of Rework'
+    graph.x_axis_label     = 'Categories'
+    graph.legend_font_size = 14
+    graph.legend_box_size  = 12
+    graph.marker_font_size = 14
+    
+    graph.no_data_message  = "\n\nNo Report\nCards\nRetrieved"
+    
+    categories = OiCategory.list
+    categories.each { |c| graph.labels[c.id-1] = c.label }
+    
+    max_value = 0
+    score_summary.each do |complexity, data|
+    
+      graph_data = []
+      0.upto(data[:count].size - 1) do |i|
+        graph_data[i] = data[:count][i] == 0 ? 0 : data[:total][i].to_f / data[:count][i]
+        max_value = graph_data[i] if graph_data[i] > max_value
+      end
+      graph.data("#{complexity} Complexity", graph_data)
+    
+    end
+    
+    # Adjust the max y value on the graph so that 
+    # bars are not all bunched around the buttom.
+    while graph.maximum_value > max_value * 2
+      graph.maximum_value /= 2
+    end
+    
+    if download
+      send_data(graph.to_blob,
+                :disposition => 'attachment',
+                :type        => 'image/png',
+                :filename    => "#{graph.title}.png")
+    else
+      graph.write('public/images/graphs/lcr_process_report.png')
+    end
+  
   end
   
   
