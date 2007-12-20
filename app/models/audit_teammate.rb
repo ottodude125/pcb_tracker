a721ac3eab2c935d18bdd00d477110c31d730e6f
altimont <>
2007-12-20 03:26:23 +0000

    Added new_teammate()

--- app/models/audit_teammate.rb
+++ app/models/audit_teammate.rb
@@ -16,4 +16,40 @@ class AuditTeammate < ActiveRecord::Base
   belongs_to :section
   belongs_to :user
   
+  
+  ##############################################################################
+  #
+  # Class Methods
+  # 
+  ##############################################################################
+
+  
+  ######################################################################
+  #
+  # new_teammate
+  #
+  # Description:
+  # 
+  #
+  # Parameters:
+  # None
+  #
+  # Return value:
+  # The teammate record that was created
+  #
+  ######################################################################
+  #
+  def self.new_teammate(audit_id, section_id, user_id, auditor, save = true)
+    
+    # Create a new teammate and save it in the database.
+    teammate = AuditTeammate.new( :audit_id   => audit_id,
+                                  :section_id => section_id,
+                                  :user_id    => user_id,
+                                  :self       => auditor == :self ? 1 : 0)
+    teammate.save if save
+    teammate
+    
+  end
+
+
 end
