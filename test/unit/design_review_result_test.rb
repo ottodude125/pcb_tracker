1758ffbe85ecda08ee70b5ec25987584970fd472
altimont <>
2007-12-23 22:04:36 +0000

    Added unit tests. dos2unix

--- test/unit/design_review_result_test.rb
+++ test/unit/design_review_result_test.rb
@@ -1,11 +1,28 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: design_review_result_test.rb
+#
+# This file contains the unit tests for the design review result model
+#
+# Revision History:
+#   $Id$
+#
+########################################################################
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class DesignReviewResultTest < Test::Unit::TestCase
+  
+  
   fixtures :design_review_results
 
+
   def setup
-    @design_review_result = DesignReviewResult.find(1)
+    @mx234a_pre_artwork_hw = design_review_results(:mx234a_pre_artwork_hw)
   end
+
 
   ###################################################################
   def test_complete
@@ -22,4 +39,32 @@ class DesignReviewResultTest < Test::Unit::TestCase
     end
     
   end
+  
+  
+  ###################################################################
+  def test_positive_response
+    
+    results = { 'APPROVED' => true,
+                'WAIVED'   => true,
+                'COMMENT'  => false, 
+                'REJECTED' => false }
+                
+    drr = DesignReviewResult.new
+    results.each do |result, expected_return|
+      drr.result = result
+      assert_equal(expected_return, drr.positive_response?)
+    end
+    
+  end
+  
+  
+  ###################################################################
+  def test_reviewer
+    assert_equal('Lee Schaff', @mx234a_pre_artwork_hw.reviewer.name)
+    
+    new_drr = DesignReviewResult.new
+    assert_nil(new_drr.reviewer)
+  end
+
+
 end
