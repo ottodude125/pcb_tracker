57e6d2678215d129713269cf1fabbda8fa036079
altimont <>
2008-01-08 21:28:39 +0000

    Deleted the edit, create, and update actions.  They are no longer used.

--- app/controllers/prefix_controller.rb
+++ app/controllers/prefix_controller.rb
@@ -18,68 +18,6 @@ class PrefixController < ApplicationController
 
   ######################################################################
   #
-  # update
-  #
-  # Description:
-  # This method uses information passed back from the edit screen to
-  # update the database.
-  #
-  # Parameters from params
-  # ['prefix'] - Used to identify the prefix to be updated.
-  #
-  # Return value:
-  # None
-  #
-  ######################################################################
-  #
-  def update
-
-    @prefix = Prefix.find(params[:prefix][:id])
-
-    if @prefix.update_attributes(params[:prefix])
-      flash['notice'] = 'Prefix was successfully updated.'
-    else
-      flash['notice'] = 'Prefix not updated'
-    end
-
-    redirect_to(:action => 'edit',
-                :id     => params[:prefix][:id])
-  end
-
-
-  ######################################################################
-  #
-  # create
-  #
-  # Description:
-  # This method uses the information passed back from the user
-  # to create a new prefix in the database
-  #
-  # Parameters from params
-  # ['new_prefix'] - the information to be stored for the new prefix.
-  #
-  # Return value:
-  # None
-  #
-  ######################################################################
-  #
-  def create
-    
-    @prefix = Prefix.create(params[:new_prefix])
-    
-    if @prefix.errors.empty?
-      flash['notice'] = "Prefix #{@prefix.pcb_mnemonic} added"
-      redirect_to :action => 'list'
-    else
-      flash['notice'] = @prefix.errors.full_messages.pop
-      redirect_to :action => 'add'
-    end
-    
-  end
-
-
-  ######################################################################
-  #
   # list
   #
   # Description:
@@ -100,30 +38,6 @@ class PrefixController < ApplicationController
   def list
     
     @prefixes = Prefix.get_prefixes
-
-  end
-
-
-  ######################################################################
-  #
-  # edit
-  #
-  # Description:
-  # This method retrieves the prefix from the database for display.
-  #
-  # Parameters from params
-  # ['id'] - Used to identify the prefix to be retrieved.
-  #
-  # Return value:
-  # None
-  #
-  # Additional information:
-  #
-  ######################################################################
-  #
-  def edit 
-
-    @prefix = Prefix.find(params[:id])
 
   end
 
