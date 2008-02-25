937cee62b15aa9b2495d188aa6ba5b548acd331b
altimont <>
2008-02-25 22:38:08 +0000

    Added a file header. Added logic to set variables based on the value of ENV['RAILS_ENV']

--- app/models/pcbtr.rb
+++ app/models/pcbtr.rb
@@ -1,12 +1,29 @@
+########################################################################
+#
+# Copyright 2005, by Teradyne, Inc., Boston MA
+#
+# File: pcbtr.rb
+#
+# This file contains general information about the tracker.
+#
+# $Id$
+#
+########################################################################
+
 class Pcbtr < ActiveRecord::Base
 
 MESSAGES = {
   :admin_only => 'Administrators only!  Check your role.'
 }
 
-PCBTR_BASE_URL = 'http://linuxolten.icd.teradyne.com/tracker/'
-SENDER         = 'PCB_Tracker'
+if ENV['RAILS_ENV'] == 'production'
+  PCBTR_BASE_URL = 'http://boarddev.teradyne.com/pcbtr/'
+  SENDER         = 'PCB_Tracker'
+  DEVEL_SERVER   = false
+else
+  PCBTR_BASE_URL = 'http://' + ENV['HOSTNAME'] + ':8000/'
+  SENDER         = 'DEVEL_PCB_TRACKER'
+end
 EAVESDROP      = 'paul_altimonte@notes.teradyne.com'
-DEVEL_SERVER   = false
 
 end
