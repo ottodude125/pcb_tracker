c2991733c64e39ecb847339040b91ce43fce778b
altimont <>
2008-10-21 12:01:04 +0000

    Added the note to indicate how to apply.

--- app/models/session_cleaner.rb
+++ app/models/session_cleaner.rb
@@ -18,6 +18,10 @@ class SessionCleaner
   #   SessionCleaner.remove_stale_sessions(hours) -> boolean
   #
   # Returns a list of incomplete audits as an array.
+  #
+  # NOTE: Invoked from crontab every 15 minutes
+  #       * 15 * * * ruby <full path>/script/runner -e production "SessionCleaner.remove_stale_sessions(8)"
+  #
   def self.remove_stale_sessions(number_of_hours)
     CGI::Session::ActiveRecordStore::Session.destroy_all(['updated_at < ?', number_of_hours.hours.ago])
   end
