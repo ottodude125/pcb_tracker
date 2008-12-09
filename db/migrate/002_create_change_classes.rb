9b33d8a2f76d9872e8ab231028575a7eb7e9b209
altimont <>
2008-12-09 20:23:36 +0000

    Added the manager_only flag to the change_classes table.

--- db/migrate/002_create_change_classes.rb
+++ db/migrate/002_create_change_classes.rb
@@ -5,6 +5,7 @@ class CreateChangeClasses < ActiveRecord::Migration
     create_table :change_classes do |t|
       t.string :name
       t.integer :position
+      t.boolean :manager_only
       t.boolean :active
       t.text    :definition
 
