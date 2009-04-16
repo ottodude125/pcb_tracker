ab307df28965da00951d26f0b4a24b230a0f5da4
altimont <>
2009-04-16 17:59:03 +0000

    Modifed display_threads() to use the time formattting methods.

--- app/helpers/ipd_post_helper.rb
+++ app/helpers/ipd_post_helper.rb
@@ -10,7 +10,7 @@ module IpdPostHelper
                                    :id     => thread.id) +
                            content_tag('span', 
                                        " by #{thread.user.name} &middot;" +
-                                       " #{thread.created_at.strftime('%d-%b-%y, %I:%M %p')}"),
+                                       " #{thread.created_at.format_dd_mon_yy('timestamp')}"),
                                        'style' => "margin:5px 0px; padding-left:#{thread.depth*20}px")
     end
     content
