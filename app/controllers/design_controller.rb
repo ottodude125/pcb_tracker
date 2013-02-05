ee2e64c907cfcebd3891320f91b3a4b710ed3940
dallas <dron.dallas@teradyne.com>
2013-02-05 16:00:02 -0500

    DSR #9214 - fixed mistake in where I entered the change. It worked but was in the wrong place.

--- app/controllers/design_controller.rb
+++ app/controllers/design_controller.rb
@@ -14,9 +14,9 @@
 
 class DesignController < ApplicationController
 
-  before_filter(:change_cc_list,
-		:verify_admin_role, 
-                :except => [:design_review_reviewers,
+  before_filter(:verify_admin_role, 
+                :except => [:change_cc_list,
+                            :design_review_reviewers,
                             :pcb_mechanical_comments, 
                             :process_reviewer_modifications,
                             :view])
