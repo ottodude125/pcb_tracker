75fd9e2ea0e58c234997929303bb42b6768c52f6
Jonathan Katon <jonathan.katon@teradyne.com>
2015-05-06 17:48:55 -0400

    no idea

--- db/schema.rb
+++ db/schema.rb
@@ -445,7 +445,22 @@ ActiveRecord::Schema.define(:version => 20150422191252) do
     t.datetime "updated_at",                   :null => false
   end
 
+  create_table "fab_deliverables2", :force => true do |t|
+    t.string   "name",                         :null => false
+    t.boolean  "active",     :default => true
+    t.integer  "parent_id"
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+  end
+
   create_table "fab_failure_modes", :force => true do |t|
+    t.string   "name",                         :null => false
+    t.boolean  "active",     :default => true
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+  end
+
+  create_table "fab_failure_modes2", :force => true do |t|
     t.string   "name",                         :null => false
     t.boolean  "active",     :default => true
     t.datetime "created_at",                   :null => false
@@ -474,6 +489,25 @@ ActiveRecord::Schema.define(:version => 20150422191252) do
     t.integer  "fab_failure_mode_id"
     t.datetime "created_at",                              :null => false
     t.datetime "updated_at",                              :null => false
+  end
+
+  create_table "fab_issues2", :force => true do |t|
+    t.string   "description"
+    t.string   "cause"
+    t.string   "resolution"
+    t.boolean  "documentation_issue",       :default => false
+    t.date     "date_received"
+    t.boolean  "clean_up_reqd",             :default => false
+    t.date     "clean_up_complete_date"
+    t.boolean  "corrected_b4_pre_prod_rel", :default => false
+    t.boolean  "full_rev_reqd",             :default => false
+    t.boolean  "bare_brd_change_reqd",      :default => false
+    t.integer  "user_id",                                      :null => false
+    t.integer  "design_id",                                    :null => false
+    t.integer  "fab_deliverable_id",                           :null => false
+    t.integer  "fab_failure_mode_id"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
   end
 
   create_table "ftp_notifications", :force => true do |t|
@@ -600,6 +634,20 @@ ActiveRecord::Schema.define(:version => 20150422191252) do
   end
 
   add_index "oracle_part_nums", ["number"], :name => "number"
+
+  create_table "oracle_part_nums_save", :force => true do |t|
+    t.string "number"
+    t.string "description", :limit => 80
+  end
+
+  add_index "oracle_part_nums_save", ["number"], :name => "number"
+
+  create_table "oracle_part_numsxx", :force => true do |t|
+    t.string "number",      :limit => 15
+    t.string "description", :limit => 80
+  end
+
+  add_index "oracle_part_numsxx", ["number"], :name => "number"
 
   create_table "part_numbers", :force => true do |t|
     t.string "pcb_prefix",       :limit => 3, :default => "", :null => false
