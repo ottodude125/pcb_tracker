b0b3d7aaff00790602945b2625bbae38cfbef1a4
Jonathan Katon <jonathan.katon@teradyne.com>
2016-03-15 13:37:47 -0400

    removing puts commands

--- app/mailers/model_task_mailer.rb
+++ app/mailers/model_task_mailer.rb
@@ -31,10 +31,10 @@ class ModelTaskMailer < ActionMailer::Base
   ######################################################################
   #
   def model_task_message(model_task, subject, new_task=false)
-    puts "\n\n"
-    to_list  = (Role.lcr_designers.collect { |d| d.email }).uniq
-    puts to_list
-    puts "monkey"
+    #puts "\n\n"
+    #to_list  = (Role.lcr_designers.collect { |d| d.email }).uniq
+    #puts to_list
+    #puts "monkey"
     #cc_list  = (Role.add_role_members(['Manager', 'HCL Manager', 'ECO Admin']) -
     #           to_list).uniq
 
