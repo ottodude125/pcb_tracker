e1adc04b7563c8223d929c7ae9d65a97edfbf290
Jonathan Katon <jonathan.katon@teradyne.com>
2016-08-29 13:58:01 -0400

    removing my email address

--- app/mailers/tracker_mailer.rb
+++ app/mailers/tracker_mailer.rb
@@ -66,9 +66,9 @@ class TrackerMailer < ActionMailer::Base
 
     recipients = []
     recipients += designers.collect { |u| u.email}
-    #recipients += User.where(:last_name => ["Kasting", "Light", "Michaels"]).collect { |u| u.email}
-    #recipients << "dtg@teradyne.com"
-    recipients << "jonathan.katon@teradyne.com"
+    recipients += User.where(:last_name => ["Kasting", "Light", "Michaels"]).collect { |u| u.email}
+    recipients << "dtg@teradyne.com"
+    #recipients << "jonathan.katon@teradyne.com"
     
     @part_numbers = part_numbers
     @active_designs = active_designs
