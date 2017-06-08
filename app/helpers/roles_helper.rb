61c6b6a197b1346acfbe496d72c75090e2ad28c1
Jonathan Katon <jonathan.katon@teradyne.com>
2017-06-08 16:26:20 -0400

    fixing js error on page
    When creating new js crashed when clicking Reviewer checkbox because it
    could not load reviewers for a role that didn't exist
    
    When clicking users in excluded list js crashed because could not add
    users to a role that did not exist
    
    Updated exclude/included lists to be in alphabetical order and only
    include active users

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -2,13 +2,13 @@ module RolesHelper
 
   # Method returns an array of users which are not assigned to a role
   def unassigned_users
-    users = User.all
+    users = User.where(active: 1)
     assigned = @role.users.collect
     list = []
     assigned.each do |id|
       list << User.find_by_id(id)
     end
-    available_users = users - list
+    available_users = (users - list).sort_by &:last_name
   end
   
   # Method returns an array of groups which are assigned to a role
@@ -18,7 +18,7 @@ module RolesHelper
     assigned.each do |id|
       listu << User.find_by_id(id)
     end
-    listu
+    listu.sort_by &:last_name
   end
 
 end
